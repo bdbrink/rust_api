ce6454dfeb4a6352b021adaa419d5eefc61a01b7
bdbrink <brendan.brink@outlook.com>
2023-08-01 17:31:56 -0500

    first push

---
+++ main.rs
@@ -0,0 +1,24 @@
+use actix_web::{web, App, HttpServer, Responder, HttpResponse};
+use std::io::Write;
+
+async fn handle_get() -> impl Responder {
+    HttpResponse::Ok().body("Hello from the GET endpoint!\n")
+}
+
+async fn handle_post() -> impl Responder {
+    HttpResponse::Ok().body("Hello from the POST endpoint!\n")
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    let bind_address = "127.0.0.1:8888";
+
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(handle_get))
+            .route("/", web::post().to(handle_post))
+    })
+    .bind(bind_address)?
+    .run()
+    .await
+}
